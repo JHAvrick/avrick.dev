e25957648ef2a993ad73a92def17bb103610cb1b
Joshua Avrick <josh.avrick@gmail.com>
2020-02-14 13:55:41 -0500

    added link highlighting for condensed header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {Link} from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import HeaderVariants from './header-variants';
@@ -7,16 +7,55 @@ import './header-condensed.css';
 
 function Header(props) {
   const [isCondensed, setIsCondensed] = useState(props.condensed);
-  useEffect(() => { console.log("C:" + props.condensed); setIsCondensed(props.condensed) }, [props.condensed])
+  useEffect(() => { setIsCondensed(props.condensed) }, [props.condensed])
+
+  const homeLinkEl = useRef(null);
+  const skillsLinkEl = useRef(null);
+  const projectsLinkEl = useRef(null);
+  const contactLinkEl = useRef(null);
+  const refs = [
+    { ref: homeLinkEl, posMin: 0, posMax: 0.5 },
+    { ref: skillsLinkEl, posMin: 0.5, posMax: 1.5 },
+    { ref: projectsLinkEl, posMin: 1.5, posMax: 2.5 },
+    { ref: contactLinkEl, posMin: 2.5, posMax: 3.5 }
+  ]
+  
+  const handleScroll = () => {
+    let pos = document.getElementById("app").scrollTop / window.innerHeight;
+    for (let i = 0; i < refs.length; i++){
+      let item = refs[i];
+      if (item.ref.current && pos > item.posMin && pos < item.posMax){
+        item.ref.current.style.color = "var(--fc-blue)";
+      } else if (item.ref.current)  {
+        item.ref.current.style.color = "var(--fc-primary)";
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (isCondensed) document.getElementById("app").addEventListener("scroll", handleScroll);
+    else document.getElementById("app").removeEventListener("scroll", handleScroll);
+    
+    return (() => {document.getElementById("app").removeEventListener("scroll", handleScroll)});
+    
+  }, [isCondensed])
 
   const HeaderCondensed = (
       <motion.header data-testid="header-condensed" className="header-condensed" variants={HeaderVariants.CondensedHeader.Item} initial="hidden" animate="show" exit={{ opacity: 0 }}>
         <motion.h1 variants={HeaderVariants.CondensedHeader.Item} className="header-condensed__h1" >JA</motion.h1>
         <motion.nav variants={HeaderVariants.CondensedHeader.Container} className="nav-vertical">
-          <motion.li variants={HeaderVariants.DefaultHeader.Item} className="nav__item"> <Link title="Visit About Section" to="/home"> Home </Link> </motion.li> 
-          <motion.li variants={HeaderVariants.DefaultHeader.Item} className="nav__item"> <Link title="Visit About Section" to="/skills"> Skills </Link> </motion.li> 
-          <motion.li variants={HeaderVariants.DefaultHeader.Item} className="nav__item"> <Link title="Visit About Section" to="/projects"> Projects </Link> </motion.li> 
-          <motion.li variants={HeaderVariants.DefaultHeader.Item} className="nav__item"> <Link title="Visit About Section" to="/contact"> Contact </Link> </motion.li>
+          <motion.li variants={HeaderVariants.DefaultHeader.Item} className="nav__item"> 
+            <Link title="Visit About Section" to="/home"> <span ref={homeLinkEl}> Home </span> </Link> 
+          </motion.li> 
+          <motion.li variants={HeaderVariants.DefaultHeader.Item} className="nav__item"> 
+            <Link title="Visit About Section" to="/skills"> <span ref={skillsLinkEl}> Skills </span> </Link> 
+          </motion.li> 
+          <motion.li variants={HeaderVariants.DefaultHeader.Item} className="nav__item">
+            <Link title="Visit About Section" to="/projects"> <span ref={projectsLinkEl}> Projects </span> </Link> 
+          </motion.li> 
+          <motion.li variants={HeaderVariants.DefaultHeader.Item} className="nav__item"> 
+            <Link title="Visit About Section" to="/contact"> <span ref={contactLinkEl}> Contact </span> </Link> 
+          </motion.li>
         </motion.nav>
       </motion.header>
   )
